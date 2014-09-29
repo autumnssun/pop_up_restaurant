dc3d9cc02919fe6dc368b7e0fe990e47e7677e1a
Khoa Tran <autumns.sun@gmail.com>
2014-09-29 19:06:19 +1000

    minor changes

--- app/scripts/controllers/cooking.js
+++ app/scripts/controllers/cooking.js
@@ -9,17 +9,20 @@
  */
 angular.module('restaurantApp')
     .controller('CookingCtrl', function($scope, userService, foodService, addressService) {
+        // .controller('CookingCtrl', function($scope) {
+
         $scope.food = {};
         //just some dummy data 1st
-        $scope.links=['http://img.gawkerassets.com/img/199vu342jcfpwjpg/original.jpg','http://www.shiftcomm.com/wp-content/uploads/2014/07/storymaker-best-hubble-space-telescope-images-20092-514x268.jpg'];
-        $scope.food.numberOfServe = 20;
+        $scope.links = ['http://img.gawkerassets.com/img/199vu342jcfpwjpg/original.jpg', 'http://www.shiftcomm.com/wp-content/uploads/2014/07/storymaker-best-hubble-space-telescope-images-20092-514x268.jpg'];
+        //$scope.food.numberOfServe = 20;
         // get current user
         $scope.food.cooker = userService.getUserPointer();
 
         addressService.getCurrentAddress().then(function(data) {
-            console.log(data);
+            console.log(data.decodeAddress[0].formatted_address);
             $scope.food.location = createParseGeoPoint(data.coords.latitude, data.coords.longitude);
-            $scope.food.address = data.decodeAddress[0];
+
+            $scope.food.address = data.decodeAddress[0].formatted_address;
         });
 
         //
@@ -41,8 +44,9 @@ angular.module('restaurantApp')
         $scope.selling = function(_food) {
             //appending photo array and sharedTime array
             $scope.food.photos = $scope.fileLinks;
-            $scope.food.readyTime=onTimeSet($scope.sharedDate);
+            $scope.food.readyTime = onTimeSet($scope.sharedDate);
             console.log(_food);
+            foodService.saleFood(_food);
         };
 
     });
